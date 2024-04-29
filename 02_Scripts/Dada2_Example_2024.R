d6dfb35bd9537adcb0bd308dfaf04d510bd1c266
Roberto Álvarez <robertoalvarezm@users.noreply.github.com>
2024-04-28 18:36:21 -0600

    Taxonomic assignment

--- 02_Scripts/Dada2_Example_2024.R
+++ 02_Scripts/Dada2_Example_2024.R
@@ -64,4 +64,17 @@ dim(seqtab)
 saveRDS(seqtab, file="03_Results/seqtab.RDS")
 
 
+seqtab.nochim <- removeBimeraDenovo(seqtab, method="consensus", multithread=TRUE, verbose=TRUE)
+sum(seqtab.nochim)/sum(seqtab)
 
+getN <- function(x) sum(getUniques(x))
+track <- cbind(out, sapply(dadaFs, getN), sapply(dadaRs, getN), sapply(mergers, getN), rowSums(seqtab.nochim))
+# If processing a single sample, remove the sapply calls: e.g. replace sapply(dadaFs, getN) with getN(dadaFs)
+colnames(track) <- c("input", "filtered", "denoisedF", "denoisedR", "merged", "nonchim")
+rownames(track) <- sample.names
+head(track)
+
+
+taxa <- assignTaxonomy(seqtab.nochim, "01_Raw_Data/silva_nr99_v138.1_train_set.fa.gz", multithread=TRUE)
+saveRDS(taxa, file="03_Results/taxa.RDS")
+readRDS("03_Results/taxa.RDS")
